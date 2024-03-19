251cb31e6a4dc4a77c23e01e48281341183bcb24
Ivan Begue <evansbegue@gmail.com>
2024-03-19 09:16:12 +0400

    Final Commit

---
+++ files/hnav.php
@@ -0,0 +1,96 @@
+<nav class="navbar">
+    <div class="logo_item">
+        <i class="bx bx-menu" id="sidebarOpen"></i>
+        <a href="homepage.html">
+            <img src="UTM.png" alt="">
+        </a>
+    </div>
+    
+
+    <div class="search_bar">
+        <input type="text" placeholder="Search">
+    </div>
+
+    <div class="navbar_content">
+        <i class="bi bi-grid"></i>
+        <i class='bx bx-sun' id="darkLight"></i>
+        <i class='bx bx-bell'></i>
+        <a href="#"> <!-- Add the URL you want to navigate to -->
+          <img src="images/avatar.jpg" alt=""></i>
+      </a>
+</nav>
+
+
+
+<!-- sidebar -->
+<nav class="sidebar">
+    <ul class="menu_items">
+        <div class="menu_title menu_dashboard"></div>
+        <!-- start -->
+        <li class="item">
+            <a href="form.html" class="nav_link">
+                <span class="navlink_icon">
+                    <i class="bx bx-bar-chart-alt"></i>
+                </span>
+                <span class="navlink">Dashboard</span>
+            </a>
+        </li>
+        <!-- end -->
+        <!-- start -->
+        <li class="item">
+            <a href="form1.html" class="nav_link">
+                <span class="navlink_icon">
+                    <i class="bx bx-envelope"></i>
+                </span>
+                <span class="navlink">Request Form</span>
+            </a>
+        </li>
+        
+        <li class="item">
+          <a href="#" class="nav_link">
+              <span class="navlink_icon">
+                  <i class="bx bx-info-circle"></i>
+              </span>
+              <span class="navlink">About Us</span>
+          </a>
+      </li>
+      <li class="item">
+        <a href="#" class="nav_link">
+            <span class="navlink_icon">
+                <i class="bx bx-phone"></i>
+            </span>
+            <span class="navlink">Contact Us</span>
+        </a>
+    </li>
+    <li class="item">
+      <a href="form.html" class="nav_link">
+          <span class="navlink_icon">
+              <i class="bx bx-file"></i>
+          </span>
+          <span class="navlink">Regulations</span>
+      </a>
+  </li>
+  <li class="item">
+    <a href="form.html" class="nav_link">
+        <span class="navlink_icon">
+            <i class="bx bx-exit"></i>
+        </span>
+        <span class="navlink">Log Out</span>
+    </a>
+</li>
+ 
+        <!-- end -->
+    </ul>
+
+    <!-- Sidebar Open / Close -->
+    <div class="bottom_content">
+        <div class="bottom expand_sidebar">
+            <span> Expand</span>
+            <i class='bx bx-log-in'></i>
+        </div>
+        <div class="bottom collapse_sidebar">
+            <span> Collapse</span>
+            <i class='bx bx-log-out'></i>
+        </div>
+    </div>
+</nav>
